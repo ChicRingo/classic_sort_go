d59447d99f6aa6a26eaa5b84b38ec088954a6b3c
miodawn <778942292@qq.com>
2020-08-28 12:59:47 +0800

    update

--- 1.冒泡排序/bubble.go
+++ 1.冒泡排序/bubble.go
@@ -4,12 +4,13 @@ import "fmt"
 
 // 冒泡排序
 func bubble(nums []int) []int {
-
 	numLen := len(nums)
 
 	for i := 0; i < numLen; i++ {
-		for j := 0; j < numLen-1-i; j++ { // 按相邻两个一起比较，所以最后一个不用遍历 -1,已经排序的也不用重新排序，所以-i，不写也可
-			if nums[j] > nums[j+1] { // 当前值大于后一个值，则对换位置
+		// 按相邻两个一起比较，所以最后一个不用遍历 -1,已经排序的也不用重新排序，所以-i，不写也可
+		for j := 0; j < numLen-1-i; j++ {
+			// 当前值大于后一个值，则对换位置
+			if nums[j] > nums[j+1] {
 				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
